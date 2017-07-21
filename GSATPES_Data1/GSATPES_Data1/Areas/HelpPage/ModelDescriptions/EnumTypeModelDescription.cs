b41bbe23fc6fcbc8d88b915f95ddc5e66fa4c06c
HariniGithub <HariniGithub@users.noreply.github.com>
2017-07-21 16:06:03 -0400

    Add files via upload

---
+++ GSATPES_Data1/GSATPES_Data1/Areas/HelpPage/ModelDescriptions/EnumTypeModelDescription.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace GSATPES_Data1.Areas.HelpPage.ModelDescriptions
+{
+    public class EnumTypeModelDescription : ModelDescription
+    {
+        public EnumTypeModelDescription()
+        {
+            Values = new Collection<EnumValueDescription>();
+        }
+
+        public Collection<EnumValueDescription> Values { get; private set; }
+    }
+}
